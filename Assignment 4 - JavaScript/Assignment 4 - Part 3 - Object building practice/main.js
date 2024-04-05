30d590c12040a7a54c9cf45db6ac4e083fbe80b7
Cp0wlette <156467271+Cp0wlette@users.noreply.github.com>
2024-04-05 13:08:13 -0400

    Update main.js
    Fixed top comments, added ball movement animation, and added ball collison from MDN docs

--- Assignment 4 - JavaScript/Assignment 4 - Part 3 - Object building practice/main.js
+++ Assignment 4 - JavaScript/Assignment 4 - Part 3 - Object building practice/main.js
@@ -41,7 +41,7 @@ function randomRGB() {
       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
       ctx.fill();
     }
-    
+
     update() {
       if ((this.x + this.size) >= width) {
         this.velX = -(this.velX);
@@ -62,5 +62,52 @@ function randomRGB() {
       this.x += this.velX;
       this.y += this.velY;
     }
+    collisionDetect() {
+      for (const ball of balls) {
+        if (this !== ball) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
     
+          if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+          }
+        }
+      }
+    }
   }
+
+  
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
+
