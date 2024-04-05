79a6995f86cf2c3803f64f6ece73083221ceb691
Cp0wlette <156467271+Cp0wlette@users.noreply.github.com>
2024-04-05 17:19:47 -0400

    Update main-finished.js
    Defined EvilCircle

--- Assignment 4 - JavaScript/Assignment 4 - Part 4 - Adding features to our bouncing balls demo/main-finished.js
+++ Assignment 4 - JavaScript/Assignment 4 - Part 4 - Adding features to our bouncing balls demo/main-finished.js
@@ -128,5 +128,29 @@ class Ball extends Shape {
   }
 }
 
-loop();
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = 'white'; 
+    this.size = 10; 
 
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+}
+
+loop();
